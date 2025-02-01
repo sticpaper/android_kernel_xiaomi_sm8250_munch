6532bb8ccea4ae9b81653fb43f93628702a9f06a
stic-server-open <1138705738@qq.com>
2025-02-01 19:42:11 +0800

    drivers: tp_common: Clean up unneeded nodes for xiaomi sm8250 devices
    Change-Id: Ifedcde6a3f122c57471c057b3b4471424302dd4f

--- drivers/input/touchscreen/tp_common.c
+++ drivers/input/touchscreen/tp_common.c
@@ -1,22 +1,19 @@
 #include <linux/input/tp_common.h>
 
-bool capacitive_keys_enabled;
 struct kobject *touchpanel_kobj;
 
-#define TS_ENABLE_FOPS(type)                                                   \
-	int tp_common_set_##type##_ops(struct tp_common_ops *ops)              \
-	{                                                                      \
-		static struct kobj_attribute kattr =                           \
-			__ATTR(type, (S_IWUSR | S_IRUGO), NULL, NULL);         \
-		kattr.show = ops->show;                                        \
-		kattr.store = ops->store;                                      \
-		return sysfs_create_file(touchpanel_kobj, &kattr.attr);        \
-	}
+#define TS_ENABLE_FOPS(type)                                        \
+int tp_common_set_##type##_ops(struct tp_common_ops *ops)           \
+{                                                                   \
+	static struct kobj_attribute kattr =                        \
+		__ATTR(type, (S_IWUSR | S_IRUGO), NULL, NULL);      \
+	kattr.show = ops->show;                                     \
+	kattr.store = ops->store;                                   \
+	return sysfs_create_file(touchpanel_kobj, &kattr.attr);     \
+}
 
-TS_ENABLE_FOPS(capacitive_keys)
 TS_ENABLE_FOPS(double_tap)
 TS_ENABLE_FOPS(fod_status)
-TS_ENABLE_FOPS(reversed_keys)
 
 static int __init tp_common_init(void)
 {
@@ -26,5 +23,4 @@ static int __init tp_common_init(void)
 
 	return 0;
 }
-
 core_initcall(tp_common_init);
--- include/linux/input/tp_common.h
+++ include/linux/input/tp_common.h
@@ -1,6 +1,5 @@
 #include <linux/kobject.h>
 
-extern bool capacitive_keys_enabled;
 extern struct kobject *touchpanel_kobj;
 
 struct tp_common_ops {
@@ -10,7 +9,5 @@ struct tp_common_ops {
 			 const char *buf, size_t count);
 };
 
-int tp_common_set_capacitive_keys_ops(struct tp_common_ops *ops);
 int tp_common_set_double_tap_ops(struct tp_common_ops *ops);
 int tp_common_set_fod_status_ops(struct tp_common_ops *ops);
-int tp_common_set_reversed_keys_ops(struct tp_common_ops *ops);
